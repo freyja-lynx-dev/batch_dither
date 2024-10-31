3c431a5bc51f483aae3c8e7812707ac37c50c4de
Sasha Zurek <github.com.tidy739@passinbox.com>
2024-10-30 18:42:05 -0700

    stub dithering function and extra args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
-use magick_rust::{self, magick_wand_genesis, MagickError, MagickWand};
+use magick_rust::{self, bindings, magick_wand_genesis, MagickError, MagickWand};
 use std::fs;
+use std::path;
 use std::sync::Once;
 
 static START: Once = Once::new();
@@ -21,21 +22,42 @@ struct Config {
     #[arg(short, long, default_value_t = true)]
     dither: bool,
     #[arg(long, default_value_t = String::from("netsafe"))]
-    dither_map: String,
+    color_map: String,
+    #[arg(long, default_value_t = String::from("floyd"))]
+    dither_type: String,
+    /* TO-DO -- see if there's a way to provide defaults for this */
+    #[arg(long)]
+    input_dir: path::PathBuf,
+    #[arg(long)]
+    output_dir: path::PathBuf,
 }
 
-fn resize(filepath: &str, desired_size: &Config) -> Result<Vec<u8>, MagickError> {
+fn resize(filepath: &str, config: &Config) -> Result<Vec<u8>, MagickError> {
     START.call_once(|| {
         magick_wand_genesis();
     });
     let wand = MagickWand::new();
     wand.read_image(filepath)?;
-    wand.fit(desired_size.image_x, desired_size.image_y);
-    wand.write_image_blob(desired_size.output_format.as_str())
+    wand.fit(config.image_x, config.image_y);
+    wand.write_image_blob(config.output_format.as_str())
+}
+/* STUB: does not do anything */
+fn dither(filepath: &str, config: &Config) -> Result<Vec<u8>, MagickError> {
+    START.call_once(|| {
+        magick_wand_genesis();
+    });
+    let _dtype = match config.dither_type.as_str() {
+        "floyd" => bindings::DitherMethod_FloydSteinbergDitherMethod,
+        _ => bindings::DitherMethod_UndefinedDitherMethod,
+    };
+    let wand = MagickWand::new();
+    wand.read_image(filepath)?;
+    wand.write_image_blob(config.output_format.as_str())
 }
 
 fn main() {
     let config = Config::parse();
+
     match resize("test.jpg", &config) {
         Ok(bytes) => {
             fs::write(format!("test.{}", config.output_format), bytes).expect("write failed");
